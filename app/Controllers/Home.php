c9d00154e21e5e3c7117ad23f8daac9367b0ecc5
Saber Adraoui <saberadraoui@gmail.com>
2023-06-24 23:19:54 +0100

    mail

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,7 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('index');
+        // Load the appropriate view file for your homepage
+        return view('home');
     }
 }
